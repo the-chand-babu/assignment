1662e12d60a7a33ad06d2af6a99cf06ca5ca3969
chand babu <jafri.nbj@gmail.com>
2023-05-02 22:50:11 +0530

    dropdown

---
+++ dropDown/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import InputWIthSearch from './InputSearch';
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <InputWIthSearch />
+    </>
+  )
+}
+
+export default App
