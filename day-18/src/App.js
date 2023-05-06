445d7e241d031473b4930b3324f3450f2b3c4f7b
chand babu <jafri.nbj@gmail.com>
2023-05-06 22:19:02 +0530

    rem convirter

--- day-18/src/App.js
+++ day-18/src/App.js
@@ -1,80 +1,15 @@
 //https://jsonplaceholder.typicode.com/posts
 
 import React from "react";
-import './App.css';
-import { useContext, useEffect } from "react";
-import { createContext, useReducer, useState } from "react";
+import "./App.css";
+import PxToRemConverter from "./pixel";
 
-const reducer = (state, action) => {
-  if (action.type == "Postdata") {
-    return action.payload;
-  }
-};
 
-const userData = createContext();
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, []);
-  const [Ele, setEle] = useState("");
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((resp) => resp.json())
-      .then((res) => {
-        dispatch({
-          type: "Postdata",
-          payload: res,
-        });
-      });
-  }, []);
-
-  let value = {
-    state,
-    ele: Ele,
-    setEle: setEle,
-  };
-
   return (
-    <div className="App">
-     
-      <userData.Provider value={value}>
-        <POSTList />
-        <POSTDETAIL />
-      </userData.Provider>
+    <div className="detail-container">
+      <PxToRemConverter />
     </div>
   );
 }
-
-const POSTList = () => {
-  const { state, setEle } = useContext(userData);
-  console.log(state);
-
-  const handlelist = (ele) => {
-    setEle(ele);
-  };
-
-  return (
-    <ul className="ul-list">
-      {state.map((ele) => {
-        return <li onClick={() => handlelist(ele)}>{ele.title}</li>;
-      })}
-    </ul>
-  );
-};
-
-const POSTDETAIL = () => {
-  const data = useContext(userData);
-  const list = data.ele;
-
-  return (
-    <div className="detail-container">
-    <ul className="ul-body">
-      <li className="list-id">{list.id}</li>
-
-      <li className="list-userID">{list.userId}</li>
-      <li className="list-title">{list.title}</li>
-      <li className="list-body">{list.body}</li>
-    </ul>
-    </div>
-  );
-};
