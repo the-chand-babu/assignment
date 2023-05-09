934946177b403dd5b9b0c7fb96aa4b09ad3c0d02
chand babu <jafri.nbj@gmail.com>
2023-05-09 22:31:43 +0530

    button component

---
+++ day-18/src/component/Button.js
@@ -0,0 +1,13 @@
+
+const Button=()=>{
+
+    const handleSubmit=()=>{
+        alert("hello world")
+    }
+
+    return(
+        <button onClick={handleSubmit}>Submit</button>
+    )
+}
+
+export default Button;
